d0ee1c2bbfc0c76fd16deb317cafca55750aba2a
Ed Lowther <edlowther@users.noreply.github.com>
2021-11-18 09:22:47 +0000

    Adds reference to guidance on "personal data"

--- app/questions.js
+++ app/questions.js
@@ -3,7 +3,7 @@
 
   exports.nodes = [
     {
-      'text': 'Are you obtaining personal data directly?',
+      'text': 'Are you obtaining personal data directly? (See below for guidance on meaning of "personal data")',
       'edges': {
         'Yes': ['I am obtaining personal data directly', 'next'],
         'No':  ['I am not obtaining personal data directly', 'move 3']
